3e6d1eb924baefccb1f1f764ffdf0ac6dfcdd350
brennanelson <brenna@basicinvite.com>
2022-10-13 08:41:19 -0600

    Removing old files.

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { GA4Manager } from "./GA4Manager";
-export default GA4Manager;
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const GA4Manager_1 = require("./GA4Manager");
-exports.default = GA4Manager_1.GA4Manager;
